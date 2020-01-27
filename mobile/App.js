2a21b343832d8d14ed885d5eb3c539193157c060
Levi Costa <lewi.santos@outlook.com>
2020-01-27 11:38:22 -0300

    implementando campos de texto

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <TextInput style={styles.input} placeholder="Altura" keyboardType="numeric"/>
+     <TextInput placeholder="Massa" keyboardType="numeric"/>
+
     </View>
   );
 }
@@ -16,4 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    height:80,
+    textAlaign:"center",
+    width:"50%",
+    fontSize:50,
+    marginTop:24,
+  }
+
 });
